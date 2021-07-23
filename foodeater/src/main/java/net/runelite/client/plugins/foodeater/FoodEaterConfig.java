a0989951da194f1e9e179e2d4c161d406fd86c79
shroomalot <48485673+shroomalot@users.noreply.github.com>
2021-07-23 18:15:58 +0200

    Update FoodEaterConfig.java

--- foodeater/src/main/java/net/runelite/client/plugins/foodeater/FoodEaterConfig.java
+++ foodeater/src/main/java/net/runelite/client/plugins/foodeater/FoodEaterConfig.java
@@ -9,21 +9,30 @@ public interface FoodEaterConfig extends Config
 {
 
 	@ConfigItem(
-		keyName = "foodToEat",
-		name = "Food",
+		keyName = "foodToEat1",
+		name = "Food 1",
 		description = "the exact name of the food to eat (not case sensitive)",
 		position = 1
 	)
-	default String foodToEat()
+	default String foodToEat1()
 	{
 		return "lobster";
 	}
-
+	@ConfigItem(
+		keyName = "foodToEat2",
+		name = "Food 2",
+		description = "the exact name of the food to eat (not case sensitive)",
+		position = 2
+	)
+	default String foodToEat2()
+	{
+		return "Cooked karambwan"
+	}
 	@ConfigItem(
 		keyName = "minimumHealth",
 		name = "Minimum Health",
 		description = "The minimum health to eat at",
-		position = 2
+		position = 3
 	)
 	default int minimumHealth()
 	{
