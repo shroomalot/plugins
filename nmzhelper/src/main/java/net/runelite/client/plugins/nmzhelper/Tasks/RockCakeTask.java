9547af223b4f792cb020b1cea92861e299688496
shroomalot <48485673+shroomalot@users.noreply.github.com>
2021-07-31 10:26:33 +0200

    Update RockCakeTask.java

--- nmzhelper/src/main/java/net/runelite/client/plugins/nmzhelper/Tasks/RockCakeTask.java
+++ nmzhelper/src/main/java/net/runelite/client/plugins/nmzhelper/Tasks/RockCakeTask.java
@@ -46,7 +46,7 @@ public class RockCakeTask extends Task {
 
         if (inventoryWidget.getWidgetItems()
                 .stream()
-                .filter(item -> item.getId() == ItemID.DWARVEN_ROCK_CAKE_7510)
+                .filter(item -> item.getId() == ItemID.DWARVEN_ROCK_CAKE_7509)
                 .collect(Collectors.toList()).isEmpty())
             return false;
 
